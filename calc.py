0ded6f9df194b315a2bc057454699ef68fc701c7
VaibhaV-R-N <91965015+VaibhaV-R-N@users.noreply.github.com>
2022-10-24 11:54:50 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,7 +1,7 @@
 def add(a,b):
   return a+b
 
-def subb(a,b):
+def sub(a,b):
   return a-b
 
 def mult(a,b):
@@ -9,7 +9,51 @@ def mult(a,b):
 
 def div(a,b):
   if b == 0:
-     print('Division error!')
+     print('Division Error!')
      return
   return a/b
+
+def addM(arr):
+  sum = 0
+  for i in arr:
+    if !isnumeric(i):
+      print('what the fuk is wron with u!')
+      return
+    sum = sum + i
+  return sum
+
+def subM(arr):
+  result = 0
+  for i in arr:
+    if !isnumeric(i):
+      print('what the fuk is wron with u!')
+      return
+    result = result - i
+  return result
+
+def multM(arr):
+  product = 1
+  for i in arr:
+    if !isnumeric(i):
+      print('what the fuk is wron with u!')
+      return
+    product = product * i
+  return product
+
+def divM(arr):
+  result = arr[0]
+  if arr[0] == 0
+    return 0
+  for i in range(1,len(arr)):
+    if !isnumeric(arr[i]):
+      print('what the fuk is wron with u!')
+      return
+    if arr[i] == 0:
+      print('Division Error!')
+    result = result / arr[i]
+  return result
+  
+  
+  
+  
     
